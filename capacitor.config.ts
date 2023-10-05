81dc16a3b8bb3f67925f8b2c7ad5cc804e20a04c
ailu-torreval <89412378+ailu-torreval@users.noreply.github.com>
2023-10-05 15:45:26 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'merchant_CMS',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
